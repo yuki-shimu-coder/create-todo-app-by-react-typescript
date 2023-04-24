9cfc9fe2604eb391bee64c482c4b0c666d82d6db
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-25 08:42:09 +0900

    基本的な型定義の記述の仕方

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// マウスをホーバーすることで型推論による型を確認できる
+	let usrName = "name";
+	let dummyNum = 2;
+	let bool = true;
+	// 方の定義は変数宣言の後に:で付与する
+	let arry: boolean[] = [true, false, true];
+
+	// オブジェクトの定義
+	interface Name {
+		first: string;
+		//疑問符を付与することで、ない場合はないものとして扱う
+		middle?: string;
+		// | で許容する型を追加することができる。
+		last: string | null;
+	}
+
+	const name1: Name = {
+		first: "taro",
+		last: "yamada",
+	};
+
+	const name2: Name = {
+		first: "tanaka",
+		middle: "Tom",
+		last: "taro",
+	};
+
+	// 関数宣言 () : [リターン値の型を定義する事ができる]
+	const func1 = (x: number, y: number): number => {
+		return x + y;
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
