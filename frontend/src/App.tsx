c8873833dca5f735a5177b9bfd825785f7eef2f5
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-04-25 09:02:12 +0900

    Intersection Types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-	// マウスをホーバーすることで型推論による型を確認できる
+	/** マウスをホーバーすることで型推論による型を確認できる */
 	let usrName = "name";
 	let dummyNum = 2;
 	let bool = true;
-	// 方の定義は変数宣言の後に:で付与する
+	/** 型の定義は変数宣言の後に:で付与する */
 	let arry: boolean[] = [true, false, true];
 
-	// オブジェクトの定義
+	/** オブジェクトの型定義 */
 	interface Name {
 		first: string;
 		//疑問符を付与することで、ない場合はないものとして扱う
@@ -18,21 +18,40 @@ function App() {
 		// | で許容する型を追加することができる。
 		last: string | null;
 	}
-
 	const name1: Name = {
 		first: "taro",
 		last: "yamada",
 	};
-
 	const name2: Name = {
 		first: "tanaka",
 		middle: "Tom",
 		last: "taro",
 	};
 
-	// 関数宣言 () : [リターン値の型を定義する事ができる]
+	/** 関数宣言 () : [リターン値の型を定義する事ができる] */
 	const func1 = (x: number, y: number): number => {
 		return x + y;
+	};
+
+	/**
+	 * Intersection Types
+	 * Intersection Types（交差型）は、TypeScriptで複数の型を結合するために使用される型の1つです。
+	 */
+	type PROFILE = {
+		age: number;
+		city: string;
+	};
+	type LOGIN = {
+		username: string;
+		password: string;
+	};
+	/** USER 型は、age プロパティ、city プロパティ、username プロパティ、および password プロパティを持つオブジェクトを表します。 */
+	type USER = PROFILE & LOGIN;
+	const userA: USER = {
+		age: 30,
+		city: "sapporo",
+		username: "tanaka taro",
+		password: "password",
 	};
 
 	return (
