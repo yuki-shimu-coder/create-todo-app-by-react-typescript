b05faf8d85d6f9e4e689a0164e2a6d3aa9e128dd
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-05-16 07:56:41 +0900

    タスク追加機能を実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,9 @@ import "./App.css";
 import "./styles.css";
 
 function App() {
+	// 入力値のstate
+	const [todoText, setTodoText] = useState<string>("");
+
 	// 未完了TODOリストのstate
 	const [incompleteTodos, setIncompleteTodos] = useState<string[]>([
 		"aaaa",
@@ -11,13 +14,39 @@ function App() {
 	]);
 
 	// 完了TODOリストのstate
-  const [completeTodos, setCompleteTodos] = useState<string[]>(["uuuu","eeee"]);
+	const [completeTodos, setCompleteTodos] = useState<string[]>([
+		"uuuu",
+		"eeee",
+	]);
+
+	//入力イベントを定義
+	const onChangeTodoText = (e: React.ChangeEvent<HTMLInputElement>) => {
+		//引数に,HTMLのinput要素の変更イベントに対する型を定義
+		setTodoText(e.target.value);
+	};
+
+  // 追加ボタンのイベントを定義
+  const onClickAdd = () => {
+    // 未入力の場合は処理を終了
+    if(todoText === "") return;
+    // 未完了のTODOリストをコピーして、新しいタスクを追加
+    const newTodos = [...incompleteTodos, todoText];
+    // 生成した配列を未完了TODOリストとしてセット
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  }
+
 	return (
 		<>
 			{/* タスク入力エリア */}
 			<div className="input-area">
-				<input type="text" placeholder="TODOを入力" />
-				<button>追加</button>
+				<input
+					type="text"
+					placeholder="TODOを入力"
+					value={todoText}
+					onChange={onChangeTodoText}
+				/>
+				<button onClick={onClickAdd}>追加</button>
 			</div>
 			{/* 未完了エリア */}
 			<div className="incomplete-area">
