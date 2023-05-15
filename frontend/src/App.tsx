04fa9e0339e222705c1c416e8678082326edc94d
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-05-16 06:49:07 +0900

    TODOアプリの外観作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// 未完了TODOリストのstate
+	const [incompleteTodos, setIncompleteTodos] = useState<string[]>([
+		"aaaa",
+		"iiii",
+	]);
+
+	// 完了TODOリストのstate
+  const [completeTodos, setCompleteTodos] = useState<string[]>(["uuuu","eeee"]);
+	return (
+		<>
+			{/* タスク入力エリア */}
+			<div className="input-area">
+				<input type="text" placeholder="TODOを入力" />
+				<button>追加</button>
+			</div>
+			{/* 未完了エリア */}
+			<div className="incomplete-area">
+				<p className="title">未完了のTODO</p>
+				<ul>
+					{incompleteTodos.map((incompleteTodo: string) => {
+						return (
+							<li key={incompleteTodo}>
+								<div className="list-row">
+									<span>{incompleteTodo}</span>
+									<button>完了</button>
+									<button>削除</button>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			{/* 完了エリア */}
+			<div className="complete-area">
+				<p className="title">完了のTODO</p>
+				<ul>
+					{completeTodos.map((completeTodo: string) => {
+						return (
+							<li key={completeTodo}>
+								<div className="list-row">
+									<span>{completeTodo}</span>
+									<button>完了</button>
+									<button>削除</button>
+								</div>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</>
+	);
 }
 
 export default App;
