8df562a8d84a39b96e408a32d12aa178a28047d9
github yuki-shimu-coder <yuyuyou7@gmail.com>
2023-06-03 12:41:46 +0900

    useStateの型定義の説明を追加

--- frontend/src/TestComponent.tsx
+++ frontend/src/TestComponent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
 	text: string;
@@ -8,12 +8,24 @@ interface Props {
  * TypeScriptを使用したプロップスの型定義
  * 型を使用することで、開発者が意図しない値がコンポーネントに渡されるのを防ぐことができる
  * @param props text以外のpropsを受け取らない
- * @returns 
+ * @returns
  */
 const TestComponet: React.FC<Props> = (props) => {
+	/** ジェネリクスで型を指定することもできる */
+	const [count, setCount] = useState<number | null>(0);
+
+	/** useStateでオブジェクトの型を指定したい場合 */
+	interface UserData {
+		id: number;
+		name: string;
+		age: number;
+	}
+	const [user, setUser] = useState<UserData>({ id: 1, name: "taro", age: 20 });
+
 	return (
 		<div>
 			<p>{props.text}</p>
+			<p>{count}</p>
 		</div>
 	);
 };
